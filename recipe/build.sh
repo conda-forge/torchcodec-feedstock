93465aa5152f864fbb18fed3c30871fcfd49eef3
Silvio Traversaro <silvio@traversaro.it>
2025-04-27 16:14:28 +0200

    Add C++ tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,14 @@ else
    export ENABLE_CUDA=0
 fi
 
+# Also run C++ tests
+mkdir build_cxx_tests
+cd build_cxx_tests
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=1 $CMAKE_ARGS ..
+cmake --build . -- VERBOSE=1
+ctest --rerun-failed --output-on-failure
+cd ..
+
 # We explicitly depend on lgpl's variant of ffmpeg in the recipe.yaml to ensure that
 # we do not have license violation due to linking a GPL project
 export I_CONFIRM_THIS_IS_NOT_A_LICENSE_VIOLATION=1
