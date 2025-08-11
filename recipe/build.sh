d5c133e3b384733ae38d0a951ef3b7e1e09cc57c
Tobias Fischer <info@tobiasfischer.info>
2025-08-11 15:01:08 +1000

    TORCHCODEC_DISABLE_COMPILE_WARNING_AS_ERROR=1

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,8 @@ else
    export ENABLE_CUDA=0
 fi
 
+export TORCHCODEC_DISABLE_COMPILE_WARNING_AS_ERROR=1
+
 # We explicitly depend on lgpl's variant of ffmpeg in the recipe.yaml to ensure that
 # we do not have license violation due to linking a GPL project
 export I_CONFIRM_THIS_IS_NOT_A_LICENSE_VIOLATION=1
