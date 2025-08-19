a039130382d991ddbcaa78192bca0448eda8258c
Silvio Traversaro <silvio@traversaro.it>
2025-08-19 09:26:02 +0200

    Update build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -7,6 +7,7 @@ if not "%cuda_compiler_version%" == "None" (
 :: We explicitly depend on lgpl's variant of ffmpeg in the recipe.yaml to ensure that
 :: we do not have license violation due to linking a GPL project
 set I_CONFIRM_THIS_IS_NOT_A_LICENSE_VIOLATION=1
+set TORCHCODEC_DISABLE_COMPILE_WARNING_AS_ERROR=ON
 
 pip install . --no-deps --no-build-isolation -vv
 if %ERRORLEVEL% neq 0 exit 1
